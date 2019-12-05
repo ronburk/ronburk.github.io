e72a8645975af4400d4b79d2d48f389585798393
ronburk <RonBurk@peoplewideweb.org>
2019-12-04 20:23:11 -0800

    Adding publish.sh

---
+++ publish.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# publish.sh - commit/publish the GitHub pages
+
+# offers to commit and push. Skip commit with a "n".
+# skip push with Ctrl-C.
+
+# YesNo(): ask user yes/no question, return true if yes
+function YesNo() {
+    declare -l Answer    # make a lower-casing variable
+    while true; do
+        read -p "$1 [Y/n]: " Answer
+        Answer=${Answer:-y}
+        case "$Answer" in
+            "y"|"yes"|"")
+                Answer="y"
+                break
+                ;;
+            "n"|"no")
+                Answer="n"
+                break
+                ;;
+        esac
+    done
+    if [ "$Answer" == "y" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+
+if git diff-index --quiet HEAD --; then
+    echo "No uncommitted changes."
+elif YesNo "Commit changes?"; then
+    read -p "Enter commit message: " message
+    git commit -m "$message"
+fi
+
+if YesNo "Push changes?"; then
+    git push -u origin master
+fi
+
